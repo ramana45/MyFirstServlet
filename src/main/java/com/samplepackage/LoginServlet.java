d027d974d8cbefd96eb0108db9533b5aaa2baa9c
ramana <ramanarajupyla@gmail.com>
2021-01-13 18:21:32 +0530

    [Ramana]Refactored : validating the username with regex pattern

--- src/main/java/com/samplepackage/LoginServlet.java
+++ src/main/java/com/samplepackage/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -26,14 +27,25 @@ public class LoginServlet extends HttpServlet {
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+
+        String userRegex = "(^[A-Z]{1}[a-z]{2,}$)";
+
+        if(Pattern.matches(userRegex, user)) {
+            if(userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either user name or password is wrong.<font>");
+                rd.include(request, response);
+            }
         } else {
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.<font>");
+            out.println("<font color=red>Invalid user name.<font>");
             rd.include(request, response);
         }
+
     }
 }
